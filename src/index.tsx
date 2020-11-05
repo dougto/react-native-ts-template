24e16fc03367df6ea8e4cc818967ba2593f174f7
Douglas Toledo <douglas22toledo@gmail.com>
2020-11-05 20:35:46 -0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App: React.FC = () => (
+  <View style={{ flex: 1 }}>
+    <Text>Hello Template</Text>
+  </View>
+);
+
+export default App;
